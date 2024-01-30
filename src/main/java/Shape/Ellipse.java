d107eb5fe94203879d018dd7ad0db56acc01c95f
leonard <leonard@renoud.ca>
2024-01-29 22:18:18 -0500

    BaseShape passe tout les tests

--- src/main/java/Shape/Ellipse.java
+++ src/main/java/Shape/Ellipse.java
@@ -11,14 +11,30 @@ public class Ellipse extends BaseShape {
      * @param heightDiameter Height of the Ellipse
      */
     public Ellipse(Double widthDiameter, Double heightDiameter) {
-        double j=0;
-        for (double i = -widthDiameter/2; i < widthDiameter/2; i+=.5) {
-            j=Math.sqrt((1-Math.pow(i,2)/Math.pow(widthDiameter,2))*Math.pow(heightDiameter,2));
-            this.add(new Point2d(i, j));
-        }
-        for (double i = -widthDiameter/2; i < widthDiameter/2; i+=.5) {
-            j=-Math.sqrt((1-Math.pow(i,2)/Math.pow(widthDiameter,2))*Math.pow(heightDiameter,2));
-            this.add(new Point2d(i, j));
+//        double j=0;
+//        for (double i = -widthDiameter/2; i < widthDiameter/2; i+=.5) {
+//            j=Math.sqrt((1-Math.pow(i,2)/Math.pow(widthDiameter,2))*Math.pow(heightDiameter,2));
+//            this.add(new Point2d(i, j));
+//        }
+//        for (double i = -widthDiameter/2; i < widthDiameter/2; i+=.5) {
+//            j=-Math.sqrt((1-Math.pow(i,2)/Math.pow(widthDiameter,2))*Math.pow(heightDiameter,2));
+//            this.add(new Point2d(i, j));
+//        }
+
+        double widthSquared = Math.pow(widthDiameter, 2);
+        double heightSquared = Math.pow(heightDiameter, 2);
+
+        for (double i = -widthDiameter/2; i < widthDiameter/2; i+=0.5) {
+
+            double iSquared = Math.pow(i, 2);
+            double denominator = widthSquared - iSquared;
+
+            double jPositive = Math.sqrt(heightSquared * (1 - iSquared/denominator));
+            double jNegative = -jPositive;
+
+            this.add(new Point2d(i, jPositive));
+            this.add(new Point2d(i, jNegative));
+
         }
     }
 
