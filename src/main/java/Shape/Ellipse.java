6b6ce45061608143f7ddfa2fd6f44a20ce51e075
leonard <leonard@renoud.ca>
2024-01-31 21:40:27 -0500

    essai de modification dans la version clean

--- src/main/java/Shape/Ellipse.java
+++ src/main/java/Shape/Ellipse.java
@@ -35,9 +35,10 @@ public class Ellipse extends BaseShape {
          double removeFactor = 0.375;
          for (double i = (-widthDiameter * radiusFactor); i <= (widthDiameter * radiusFactor); i += ecartPoint) {
              for (double j = (-heightDiameter * radiusFactor); j <= (heightDiameter * radiusFactor); j += ecartPoint) {
-                 double ellipseEquationValue=Math.pow(i,2)/Math.pow(widthDiameter/2,2)+Math.pow(j,2)/Math.pow(heightDiameter/2,2);
-                 if (ellipseEquationValue <= 1) {
-                     if(i <= (widthDiameter * removeFactor) && i > (-widthDiameter * removeFactor) && j <= (heightDiameter * removeFactor) && j > (-heightDiameter * removeFactor) ) {
+                 double ellipseEquationValueAdd = Math.pow(i,2)/Math.pow(widthDiameter/2,2)+Math.pow(j,2)/Math.pow(heightDiameter/2,2);
+                 if (ellipseEquationValueAdd <= 1) {
+                     double ellipseEquationValueRemove = Math.pow(i,2)/Math.pow(widthDiameter*3/8,2)+Math.pow(j,2)/Math.pow(heightDiameter*3/8,2);
+                     if(i <= (widthDiameter * removeFactor) && i > (-widthDiameter * removeFactor) && j <= (heightDiameter * removeFactor) && j > (-heightDiameter * removeFactor) && ellipseEquationValueRemove <= 1 ) {
                          this.remove(new Point2d(i, j));
                      }
                      else {
